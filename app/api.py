1b46f606d2395eee052d68821e724ebc3f4f03b5
Tim Henigan <tim.henigan@gmail.com>
2014-08-08 12:58:21 -0700

    implement the PostResult service
    This api endpoint allows an authenticated game server to post the
    results of a game between two active AGA members to the ratings
    database.
    
    Signed-off-by: Tim Henigan <tim.henigan@gmail.com>

--- app/api.py
+++ app/api.py
@@ -1,17 +1,79 @@
 from flask.ext.security import login_required
 from flask import jsonify, request
 from app import app, db, models
+from datetime import datetime
+
+
+def _result_str_valid(result):
+    """Check the format of a result string per the SGF file format.
+
+    See http://www.red-bean.com/sgf/ for details.
+    """
+    if result in ['0', 'Draw', 'Void', '?']:
+        return True
+
+    if result.startswith('B') or result.startswith('W'):
+        score = result[2:]
+        if score in ['R', 'Resign', 'T', 'Time', 'F', 'Forfeit']:
+            return True
+        try:
+            numeric_score = float(score)
+            return True
+        except ValueError:
+            return False
+    return False
 
 
 @app.route('/PostResult', methods=['POST'])
 def postresult():
-    """Post a new game result to the database."""
-    # input: server token, w token, b token
-    #        result, rated, date played
-    #        game record (write to file system?)
-    # output: [game added | invalid server |
-    #          invalid player w | invalid player b]
-    return jsonify(error='service not yet implemented')
+    """Post a new game result to the database.
+
+    TODO: Check for duplicates.
+    """
+    data = {
+        'server_tok': request.args.get('server_tok'),
+        'b_tok': request.args.get('b_tok'),
+        'w_tok': request.args.get('w_tok'),
+        'rated': request.args.get('rated'),
+        'result': request.args.get('result'),
+        'date': request.args.get('date')
+    }
+    if None in data.values():
+        return jsonify(error='malformed request')
+
+    gs = models.GoServer.query.filter_by(token=data['server_tok']).first()
+    if gs is None:
+        return jsonify(error='server access token unknown or expired')
+
+    b = models.GoServerAccount.query.filter_by(token=data['b_tok']).first()
+    if b is None or b.go_server_id != gs.id:
+        return jsonify(error='user access token unknown or expired')
+
+    w = models.GoServerAccount.query.filter_by(token=data['w_tok']).first()
+    if w is None or w.go_server_id != gs.id:
+        return jsonify(error='user access token unknown or expired')
+
+    if data['rated'] not in ['True', 'False']:
+        return jsonify(error='rated must be set to True or False')
+
+    if not _result_str_valid(data['result']):
+        return jsonify(error='format of result is incorrect')
+
+    try:
+        date_played = datetime.strptime(data['date'], '%Y-%m-%d')
+    except:
+        return jsonify(error='date must be of the form year-month-day')
+
+    rated = data['rated'] == 'True'
+    game = models.Game(white=w,
+                       black=b,
+                       rated=rated,
+                       date_played=date_played,
+                       date_reported=datetime.now(),
+                       result=data['result'])
+    db.session.add(game)
+    db.session.commit()
+    return jsonify(message='OK')
 
 
 @app.route('/VerifyUser', methods=['GET'])
