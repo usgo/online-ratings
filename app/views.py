958de2417fec2ae8ef628cddf4905c22278b3c00
Tim Henigan <tim.henigan@gmail.com>
2014-08-25 14:37:40 -0700

    add endpoint to list all valid endpoints
    The /api endpoint now returns a json list of all the endpoints on the
    server.
    
    Signed-off-by: Tim Henigan <tim.henigan@gmail.com>

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask.ext.security import login_required
-from flask import Blueprint, render_template, jsonify
-from .models import db, Game, GoServerAccount
+from flask import Blueprint, render_template, jsonify, current_app, url_for
+from .models import GoServerAccount
 
 ratings = Blueprint("ratings", __name__)
 
@@ -9,6 +9,19 @@ ratings = Blueprint("ratings", __name__)
 @login_required
 def home():
     return render_template('index.html')
+
+
+@ratings.route('/api')
+def api_list():
+    '''return a json list of api endpoints'''
+    urls = {}
+    for rule in current_app.url_map.iter_rules():
+        if rule.endpoint is not 'static':
+            urls[rule.endpoint] = {
+                'methods': ','.join(rule.methods),
+                'url': url_for(rule.endpoint)
+            }
+    return jsonify(urls)
 
 
 @ratings.route('/Player', methods=['GET'])
