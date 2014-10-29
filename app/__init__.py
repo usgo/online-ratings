12e53afad5f3970234faa7eb929ebac0b5bf3bf0
Tim Henigan <tim.henigan@gmail.com>
2014-10-29 09:55:13 -0700

    init the app database every time the app is started
    This feature is only intended for early development.  Every time the
    application is started, the application database is deleted and
    recreated using default data.
    
    When we get closer to release, this change must be backed out so that
    the database persists between application starts/stops.
    
    Signed-off-by: Tim Henigan <tim.henigan@gmail.com>

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,13 @@ app.config.from_object('config.DebugConfiguration')
 db.init_app(app)
 security = Security(app, user_datastore)
 app.register_blueprint(ratings)
+
+
+#TODO: remove the following init function, which destroys any existing app
+#database and replaces it with default data
+@app.before_first_request
+def init_db():
+    from .models import create_test_data
+    db.drop_all()
+    db.create_all()
+    create_test_data()
