723b317c15b9367125bfe8c039f3dfa9eda72315
Tim Henigan <tim.henigan@gmail.com>
2014-11-03 15:59:19 -0800

    only show API endpoints in API map
    Signed-off-by: Tim Henigan <tim.henigan@gmail.com>

--- app/api_1_0/api_map.py
+++ app/api_1_0/api_map.py
@@ -7,7 +7,7 @@ def api_list():
     '''return a json list of api endpoints'''
     urls = {}
     for rule in current_app.url_map.iter_rules():
-        if rule.endpoint is not 'static':
+        if rule.endpoint is not 'static' and rule.endpoint.startswith('api.'):
             urls[rule.endpoint] = {
                 'methods': ','.join(rule.methods),
                 'url': url_for(rule.endpoint)
