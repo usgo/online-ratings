b3f11d9bc0702c667a9f7ce35c242508f2b2459f
Tim Henigan <tim.henigan@gmail.com>
2014-11-07 14:15:59 -0800

    add a simple page to view a user profile
    Once users register with the site, they need to be able to find their
    token to share it with game servers.  This adds a very simple form that
    displays a user's profile.
    
    Signed-off-by: Tim Henigan <tim.henigan@gmail.com>

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from flask.ext.security import login_required
 from flask import Blueprint, render_template
+from flask.ext.login import current_user
+from flask.ext.security import login_required
 from .tokengen import UUIDTokenGenerator as TokenGenerator
 from . import db, user_datastore
 
@@ -10,6 +11,12 @@ ratings = Blueprint("ratings", __name__)
 @login_required
 def home():
     return render_template('index.html')
+
+
+@ratings.route('/ViewProfile')
+@login_required
+def viewprofile():
+    return render_template('profile.html', user=current_user)
 
 
 def user_registered_sighandler(app, user, confirm_token):
