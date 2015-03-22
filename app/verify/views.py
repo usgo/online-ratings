bf48116278c65b6b3100046b507e23031c424ca8
Andrew Jackson <andrew.m.jackson@gmail.com>
2015-03-21 23:52:17 -0700

    verify seems to work.  TODO: lookup actual e-mail and send it...

--- app/verify/views.py
+++ app/verify/views.py
@@ -1,9 +1,15 @@
 from . import verify
 
-from flask import abort, redirect, url_for
+from flask import abort, redirect, url_for, render_template, flash
+from flask.ext.security import login_required
+from flask.ext.login import current_user
+from flask.ext.wtf import Form
 from itsdangerous import BadSignature, URLSafeSerializer
-from app.models import Player
+from app.models import User
 import app, logging
+
+from wtforms import IntegerField, SubmitField
+from wtforms.validators import Required
 
 
 def get_serializer(secret_key=None):
@@ -12,21 +18,39 @@ def get_serializer(secret_key=None):
     return URLSafeSerializer(secret_key)
 
 @verify.route('/verify/<payload>')
+@login_required
 def verify_player(payload):
     s = get_serializer()
     try:
-        player_id, aga_id = s.loads(payload)
+        user_id, aga_id = s.loads(payload)
     except BadSignature:
         logging.info('Verify called with invalid paylod')
         abort(404)
 
-    player = Player.query.get_or_404(player_id)
-    #TODO: verify that aga_id == player's user's aga id.  
-    #TODO: something like player.activate(), maybe generate initial token?
-    return redirect(url_for('app.index'))
+    user = User.query.get_or_404(user_id)
+    user.aga_id = aga_id
+    #TODO: something like user.activate(), maybe generate initial token?
+    msg = 'Linked account with AGA #%d' %user.aga_id
+    #TODO: redirect to profile
+    return redirect('/')
 
-def get_verify_link(player, aga_id):
+def get_verify_link(user, aga_id):
     s = get_serializer()
-    payload = s.dumps([player.id, aga_id])
-    return url_for('verify_player', payload=payload, 
+    payload = s.dumps([user.id, aga_id])
+    return url_for('.verify_player', payload=payload, 
                    _external=True)
+
+@verify.route('/verify', methods=['GET', 'POST'])
+@login_required
+def verify_form():
+    form = LinkUserWithAGANumberForm()
+    if form.validate_on_submit():
+        aga_id = form.aga_id.data
+        link = get_verify_link(current_user, aga_id)
+        return render_template('verify/testmsg.html', link=link)
+    return render_template('verify/verifyform.html', form=form)
+
+class LinkUserWithAGANumberForm(Form):
+    aga_id = IntegerField('Aga #?',
+                          validators=[Required()])
+    submit = SubmitField()
