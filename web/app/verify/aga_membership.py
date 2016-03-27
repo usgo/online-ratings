8eeba9310818b376d784b0172df2d0b8293670c0
Brian Lee <brian.kihoon.lee@gmail.com>
2016-03-26 22:27:11 -0400

    Cleaner MM api client, better error handling

--- web/app/verify/aga_membership.py
+++ web/app/verify/aga_membership.py
@@ -1,24 +1,22 @@
+from logging import getLogger
 import os
 import requests
-from urllib.parse import urlunparse, urlencode, ParseResult
 
-MEMBERSHIP_API_FQDN = "usgo.org"
-MEMBERSHIP_API_ROOT = "/mm/api/members/"
+LOG = getLogger('aga_membership')
+MEMBERSHIP_API_URL = "https://usgo.org/mm/api/members"
 MEMBERSHIP_API_KEY = os.environ.get("AGA_MM_API_KEY", "")
-EMAIL_FIELD_NAME = "email"
 
 def get_email_address(aga_id):
-    url = urlunparse(ParseResult(
-        scheme="https",
-        netloc=MEMBERSHIP_API_FQDN,
-        path=os.path.join(MEMBERSHIP_API_ROOT, str(aga_id)),
-        params="",
-        query=urlencode({"api_key": MEMBERSHIP_API_KEY}),
-        fragment=""))
-    response = requests.get(url)
+    url = os.path.join(MEMBERSHIP_API_URL, str(aga_id))
     try:
+        response = requests.get(url, params={"api_key": MEMBERSHIP_API_KEY})
         if not response.json()['success']:
+            LOG.error("API call didn't succeed: %s", response.json())
             return None
         return response.json()['payload']['row']['email']
-    except IndexError:
+    except ValueError:
+        LOG.exception("Couldn't interpret response as json: %s", response.content)
+        return None
+    except KeyError:
+        LOG.exception("Got unexpected json format %s", response.json())
         return None
