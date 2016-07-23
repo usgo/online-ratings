846bfffd5bea7dea8d7161dbfd4b114eb4bf2280
Obtuse <theobtuseautodidact@gmail.com>
2016-07-23 11:42:39 -0700

    Add tests posting to 'edit' and 'new'

--- web/tests/test_tournament.py
+++ web/tests/test_tournament.py
@@ -23,7 +23,7 @@ class TestTournament(BaseTestCase):
     #  index
     def test_tournament_base_url(self):
         response = self.client.get(self.tournament_endpoint)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(200, response.status_code)
 
     #  show
     def test_it_returns_an_individual_tournament(self):
@@ -33,33 +33,38 @@ class TestTournament(BaseTestCase):
         self.assertTrue('LasVegas' in str(response.data))
 
     #  edit
-    # def test_it_can_edit_tournament_info(self):
-    #     tournament_edit_endpoint = self.tournament_endpoint + str(self.tournament_1.id) + '/edit'
-    #     t = Tournament.query.first()
-    #     self.assertEqual(t.event_name, self.tournament_1.event_name)
-    #     test_string = "This event name changed for testing"
-    #     t.event_name = test_string
-    #     db.session.commit()
-    #     t = Tournament.query.first()
-    #     self.assertEqual(t.event_name, test_string)
+    def test_it_can_edit_tournament_info(self):
+        tournament_edit_endpoint = self.tournament_endpoint + str(self.tournament_1.id) + '/edit'
+        t = Tournament.query.first()
+        self.assertEqual(t.event_name, self.tournament_1.event_name)
+        test_string = "This event name changed for testing"
+        t.event_name = test_string
+        response = self.client.post(
+            '/tournament/'+str(t.id)+'/edit', #  url_for()
+            data={"event_name" : t.event_name,
+                  "start_date" : t.start_date,
+                  "venue" : t.venue,
+                  "director" : t.director,
+                  "pairing" : t.pairing,
+                  "rule_set" : t.rule_set})
+        t = Tournament.query.first()
+        self.assertEqual(test_string, t.event_name)
 
     #  new
-    # def test_it_can_create_new_instance_of_tournament(self):
-    #     before = Tournament.query.count()
-    #     self.assertEqual(before, 1)
-    #     response1 = self.client.post('tournament/new',
-    #         data={'event_name': 'testing'})
-    #     print('XXXX', response1.data, 'XXXX')
-    #     # self.client.post('/tournament/new',
-    #     #     data=json.dumps({
-    #     #         "event_name": "testing "
-    #     #         }), content_type="application/json",
-    #     #         environ_base={
-    #     #             'HTTP_USER_AGENT':'Chrome',
-    #     #             'REMOTE_ADDR': '127.0.0.1'
-    #     #         })
-    #     after = Tournament.query.count()
-    #     t = Tournament.query.all()
-    #     t = t[-1]
-    #     # self.assertEqual(after, 2)
-    #     self.assertEqual(t.event_name, 'testing')
+    def test_it_can_add_a_new_tournament(self):
+        tournament_new_endpoint = self.tournament_endpoint + '/new'
+        count = Tournament.query.count()
+        self.assertEqual(1, count)
+
+        response = self.client.post(
+            '/tournament/new', #  url_for()
+            data={"event_name" : "new_event",
+                  "start_date" : datetime.datetime.now(),
+                  "venue" : "right here",
+                  "director" : "new director",
+                  "pairing" : "two on two",
+                  "rule_set" : "irish dueling"})
+        count = Tournament.query.count()
+        self.assertEqual(count, 2)
+        t = Tournament.query.all()[-1]
+        self.assertEqual("new_event", t.event_name)
