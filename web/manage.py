f2e0c67009061c8d972928939c5eddc3db023354
Davis Yoshida <davis.yoshida@gmail.com>
2016-08-15 00:09:15 -0700

    Flask once again listens on all interfaces.

--- web/manage.py
+++ web/manage.py
@@ -1,10 +1,11 @@
-from flask.ext.script import Manager
+from flask.ext.script import Manager, Server
 
 from app import get_app
 
 app = get_app('config.DockerConfiguration')
 
 manager = Manager(app)
+manager.add_command('runserver', Server(host='0.0.0.0'))
 
 if __name__ == '__main__':
 	manager.run()
