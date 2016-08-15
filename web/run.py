6b843dbc2c60e7fe9f6057a4d70de55787bb268d
Brian Lee <brian.kihoon.lee@gmail.com>
2016-08-14 23:08:27 -0400

    Replace runserver/shell with flask-script equivalents

--- web/run.py
+++
@@ -1,5 +0,0 @@
-from app import get_app
-
-app = get_app('config.DockerConfiguration')
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
--- web/shell.py
+++
@@ -1,3 +0,0 @@
-from app import get_app
-app = get_app('config.DockerConfiguration')
-app.app_context().__enter__()
