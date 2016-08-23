bf63a4024472240209596a6ca039c0be961affb9
Josh Hoak <jrhoak@gmail.com>
2016-08-22 22:12:55 -0700

    Add the commit suffix as the tag.

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,10 @@
 # Simple script to Build a docker image and push to the USGO Org's Docker hub
 # repository.
 if [ "$TRAVIS_BRANCH" == "release" ]; then
-  docker build -t usgo/online-ratings-web web/
+  IMAGE_TAG=$TRAVIS_COMMIT
+  WEB_IMAGE=usgo/online-ratings-web:$IMAGE_TAG
+  echo "Building and pushing image: $WEB_IMAGE"
+  docker build -t $WEB_IMAGE web/
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-  docker push usgo/online-ratings-web
+  docker push $WEB_IMAGE
 fi
