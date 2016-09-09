6f3d674154fe0682077788f4198929f4af3a15a1
Brian Lee <brian.kihoon.lee@gmail.com>
2016-09-09 10:42:30 -0400

    Remove internal-use-only rated field from public views

--- web/app/models.py
+++ web/app/models.py
@@ -174,7 +174,6 @@ class Game(db.Model):
             "date_played": self.date_played.isoformat(),
             "date_reported": self.date_reported.isoformat(),
             "result": self.result,
-            "rated": self.rated,
         }
 
 # Setup Flask-Security
