a1f72768ecba86ab75443f91dc44f8361f115b76
Brian Lee <brian.kihoon.lee@gmail.com>
2016-11-14 20:22:59 -0500

    more idempotent create_db scripts

--- web/scripts/create_db.py
+++ web/scripts/create_db.py
@@ -1,5 +1,4 @@
 import datetime
-import os
 import random
 import getpass
 from uuid import uuid4
@@ -9,43 +8,50 @@ from flask.ext.security.utils import encrypt_password
 
 from app.models import user_datastore, Player, GoServer, Game, User, RATINGS_ADMIN_ROLE, SERVER_ADMIN_ROLE, USER_ROLE, db
 
+sgf_data = b'(;FF[4]GM[1]SZ[19]CA[UTF-8]SO[gokifu.com]BC[ja]WC[ja]EV[54th Japanese Judan]PB[Kono Takashi]BR[8p]PW[O Meien]WR[9p]KM[6.5]DT[2015-02-26]RE[W+R];B[qd];W[dp];B[pq];W[od];B[oc];W[nc];B[pc];W[nd];B[qf];W[ec];B[jd];W[hd];B[jf];W[mg];B[jh];W[mi];B[cd];W[de];B[ce];W[df];B[cg])'
+
 def initialize_db_connections():
     db.session.remove()
     db.get_engine(current_app).dispose()
 
-def create_roles():
-    role_user = user_datastore.create_role(**USER_ROLE._asdict())
-    role_gs_admin = user_datastore.create_role(**SERVER_ADMIN_ROLE._asdict())
-    role_aga_admin = user_datastore.create_role(**RATINGS_ADMIN_ROLE._asdict())
+
+def get_or_create_roles():
+    role_user = user_datastore.find_or_create_role(**USER_ROLE._asdict())
+    role_gs_admin = user_datastore.find_or_create_role(**SERVER_ADMIN_ROLE._asdict())
+    role_aga_admin = user_datastore.find_or_create_role(**RATINGS_ADMIN_ROLE._asdict())
     db.session.commit()
     return role_user, role_gs_admin, role_aga_admin
 
-def create_user(email, password, role, **kwargs):
-    u = user_datastore.create_user(email=email, password=encrypt_password(password), **kwargs)
+def get_or_create_user(email, password, role, **kwargs):
+    u = user_datastore.get_user(email)
+    if not u:
+        u = user_datastore.create_user(email=email, password=encrypt_password(password), **kwargs)
     user_datastore.add_role_to_user(u, role)
     db.session.commit()
     return u
 
-def _create_server(u_email, u_password, s_name, s_url, s_token=None):
+def get_or_create_server(s_name, s_url, u_email, u_password, s_token=None):
     if s_token is None:
         s_token = uuid4()
     server_admin_role = user_datastore.find_role(SERVER_ADMIN_ROLE.name)
-    server_admin = create_user(u_email, u_password, server_admin_role, confirmed_at=datetime.datetime.now())
-    server = GoServer(name=s_name, url=s_url, token=s_token)
+    server_admin = get_or_create_user(u_email, u_password, server_admin_role, confirmed_at=datetime.datetime.now())
+    server = GoServer.query.filter(GoServer.name==s_name).first()
+    if not server:
+        server = GoServer(name=s_name, url=s_url, token=s_token)
     server.admins.append(server_admin)
     db.session.add(server)
     db.session.commit()
     return server
 
 def create_test_data():
-    role_user, role_gs_admin, role_aga_admin = create_roles()
-    superadmin = create_user("admin@usgo.org", "usgo", role_aga_admin, confirmed_at=datetime.datetime.now())
-    kgs_server = _create_server("admin@gokgs.com", "kgs", "KGS", "http://gokgs.com", s_token="secret_kgs")
-    ogs_server = _create_server("admin@ogs.com", "ogs", "OGS", "http://online-go.com", s_token="secret_ogs")
+    role_user, role_gs_admin, role_aga_admin = get_or_create_roles()
+    superadmin = get_or_create_user("admin@usgo.org", "usgo", role_aga_admin, confirmed_at=datetime.datetime.now())
+    kgs_server = get_or_create_server("KGS", "http://gokgs.com", "admin@gokgs.com", "kgs", s_token="secret_kgs")
+    ogs_server = get_or_create_server("OGS", "http://online-go.com", "admin@ogs.com", "ogs", s_token="secret_ogs")
 
-    foo_user = create_user("foo@foo.com", "foo", role_user, aga_id=10, confirmed_at=datetime.datetime.now())
-    bar_user = create_user("bar@bar.com", "bar", role_user, aga_id=20, confirmed_at=datetime.datetime.now())
-    baz_user = create_user("baz@baz.com", "baz", role_user, aga_id=30, confirmed_at=datetime.datetime.now())
+    foo_user = get_or_create_user("foo@foo.com", "foo", role_user, aga_id=10, confirmed_at=datetime.datetime.now())
+    bar_user = get_or_create_user("bar@bar.com", "bar", role_user, aga_id=20, confirmed_at=datetime.datetime.now())
+    baz_user = get_or_create_user("baz@baz.com", "baz", role_user, aga_id=30, confirmed_at=datetime.datetime.now())
     db.session.add(Player(id=1, name="FooPlayerKGS", server_id=kgs_server.id, user_id=foo_user.id,token="secret_foo_KGS"))
     db.session.add(Player(id=4, name="FooPlayerOGS", server_id=ogs_server.id, user_id=foo_user.id,token="secret_foo_OGS"))
     db.session.add(Player(id=2, name="BarPlayerKGS", server_id=kgs_server.id, user_id=bar_user.id,token="secret_bar_KGS"))
@@ -53,10 +59,6 @@ def create_test_data():
     db.session.add(Player(id=3, name="BazPlayerKGS", server_id=kgs_server.id, user_id=baz_user.id,token="secret_baz_KGS"))
     db.session.add(Player(id=6, name="BazPlayerOGS", server_id=ogs_server.id, user_id=baz_user.id,token="secret_baz_OGS"))
 
-
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    with open(os.path.join(basedir, '../tests/testsgf.sgf'), 'rb') as sgf_file:
-        sgf_data = sgf_file.read()
 
     db.session.add(Game(server_id=1, white_id=1, black_id=2, rated=True, result="B+0.5", game_record=sgf_data,
         date_played=datetime.datetime.now() - datetime.timedelta(seconds=random.randint(0,1000000))))
@@ -86,11 +88,7 @@ def create_test_data():
 
 
 def create_extra_data():
-    basedir = os.path.abspath(os.path.dirname(__file__))
-
     #Make a bunch of example users
-    with open(os.path.join(basedir, 'tests/testsgf.sgf')) as sgf_file:
-        sgf_data = "\n".join(sgf_file.readlines()).encode()
     role_user = user_datastore.find_role('user')
     users = []
     for i in range(60):
@@ -107,7 +105,6 @@ def create_extra_data():
     db.session.commit()
 
     users = User.query.all()
-    players = Player.query.all()
     p_priors = {user.id: random.randint(0,40) for user in users}
     print("Prior ratings")
     for p in sorted(p_priors, key=lambda k: p_priors[k]):
@@ -146,21 +143,22 @@ def drop_all_tables(force=False):
     initialize_db_connections()
     if force:
         db.drop_all()
+        db.engine.execute('DROP TABLE alembic_version;')
     else:
         print("Not dropping any tables without --force")
 
 def create_barebones_data():
     'Initialize database without any data fixtures; just a superuser account.'
     initialize_db_connections()
-    role_user, role_gs_admin, role_aga_admin = create_roles()
+    role_user, role_gs_admin, role_aga_admin = get_or_create_roles()
     admin_username = input("Enter a superuser email address > ")
     admin_password = getpass.getpass("Enter the superuser password > ")
-    superadmin = create_user(admin_username, admin_password, role_aga_admin)
+    superadmin = get_or_create_user(admin_username, admin_password, role_aga_admin, confirmed_at=datetime.datetime.now())
     db.session.commit()
 
 def create_all_data():
     'Initialize database tables, create fake servers, users, players, game data, etc.'
-    initialize_db_connections()
+    create_barebones_data()
     create_test_data()
     create_extra_data()
 
@@ -170,4 +168,4 @@ def create_server():
     server_url = input("Enter the server's URL > ")
     user_name = input("Enter an email address for the admin of this server > ")
     password = input("Enter a password for the admin")
-    _create_server(user_name, password, server_name, server_url)
+    get_or_create_server(server_name, server_url, user_name, password)
