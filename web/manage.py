bf00e5c89ecf586b898417036577928bde642785
Andrew Jackson <andrew.jackson@usgo.org>
2016-12-31 09:48:42 -0800

    change rate_all to flask-script and add it to manage.py

--- web/manage.py
+++ web/manage.py
@@ -5,6 +5,7 @@ from app import get_app
 
 from scripts.create_db import drop_all_tables, create_barebones_data, create_all_data, create_server
 from scripts.load_agagd_data import AGAHistoricalGamesLoader
+from scripts.rate_all import RatingsAtCommand
 
 app = get_app('config.DockerConfiguration')
 
@@ -22,6 +23,7 @@ def config():
     print('\n'.join('%s=%s' % item for item in sorted(app.config.items())))
 
 manager.add_command('load_agagd_data', AGAHistoricalGamesLoader)
+manager.add_command('rate_at', RatingsAtCommand)
 
 if __name__ == '__main__':
     manager.run()
